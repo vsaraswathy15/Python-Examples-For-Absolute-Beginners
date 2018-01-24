cab95644898a000dc15a5f51eb266037c12c493e
Saraswathy V <vsaraswathy15@gmail.com>
2018-01-24 21:04:28 +0530

    Accessing values in lists

---
+++ example_7.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+friends = ['Saraswathy', 'Pavithra', 'Shravanthi', 'Farheen', 'Soujanya']
+
+print "My first two friends are : ", friends[0:2]
+print "My friends are : ", friends
+print "My Last three friends are : ", friends[2:5]
+
