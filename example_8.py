aebc25062332b4525f47b8e6306e828c31071aa4
Saraswathy V <vsaraswathy15@gmail.com>
2018-01-24 21:06:18 +0530

    Updating list using if statement

---
+++ example_8.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+friends = ['Saraswathy', 'Pavithra', 'Shravanthi', 'Farheen', 'Soujanya']
+
+for friend in friends:
+    print 'My friend : ', friend
+ 
+    if friend == "Shravanthi":
+        print friend
+  
