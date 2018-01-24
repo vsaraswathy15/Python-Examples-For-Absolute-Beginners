6992eeeb6c90e4e40ed321d6b231a13c1cd3fea2
Saraswathy V <vsaraswathy15@gmail.com>
2018-01-24 21:12:06 +0530

    Implementing calculator using functions

---
+++ example_11.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+
+def sum(a,b):
+    result = a+b
+    return result
+
+
+def subtract(a,b):
+    result = a-b
+    return result
+
+
+def mul(a,b):
+    result = a*b
+    return result
+
+
+def division(a,b):
+    result = a/b
+    return result
+
+
+print("sum of two numbers are 12,6",sum(12,6))
+print("difference of two numbers are 6,5=",subtract(6,5))
+print("multiplication of two numbers are 7,8",mul(7,8))
+print("division of two numbers are 50,10",division(50,10))
+
